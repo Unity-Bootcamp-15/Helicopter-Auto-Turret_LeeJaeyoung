2ad32c057224eb8ea0a979b2ad97efe0cbd05226
namwon-RT <namwon1006@gmail.com>
2026-02-06 17:42:33 +0900

    a

---
+++ My project/Assets/NewScriptableObjectScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "a", menuName = "Scriptable Objects Assets/a")]
+public class NewScriptableObjectScript : ScriptableObject
+{
+    public int a = 1;
+    public int b = 2;
+    public int c = 3;
+
+}
